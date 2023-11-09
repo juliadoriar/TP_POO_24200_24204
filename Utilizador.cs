ef1fae5c3833fa470d8bdc352f4392532538763b
juliadoriar <juliadoriarodrigues@gmail.com>
2023-11-09 13:59:39 +0000

    Atualizar Utilizador

--- Utilizador.cs
+++ Utilizador.cs
@@ -7,7 +7,47 @@ using System.Threading.Tasks;
 namespace TP_POO_24200_24204
 {
     internal class Utilizador
-    {
-        
-    } 
+    {//generate class without using dates
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //public DateTime DataNascimento { get; set; }
+        public string DataNascimento { get; set; }
+        public string Morada { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Localidade { get; set; }
+        public string Telefone { get; set; }
+        public string NIF { get; set; }
+        public string IBAN { get; set; }
+        public string TipoUtilizador { get; set; }
+        public string Estado { get; set; }
+        public string DataRegisto { get; set; }
+        public string DataUltimoAcesso { get; set; }
+        public string DataUltimaPassword { get; set; }
+        public string DataUltimoLogin { get; set; }
+        public string DataUltimoLogout { get; set; }
+        public string DataUltimaAlteracao { get; set; }
+        public string DataUltimaEliminacao { get; set; }
+        public string DataUltimaRecuperacao { get; set; }
+        public string DataUltimaDesativacao { get; set; }
+        public string DataUltimaAtivacao { get; set; }
+        public string DataUltimaBloqueio { get; set; }
+        public string DataUltimoDesbloqueio { get; set; }
+        public string DataUltimaSessao { get; set; }
+        public string DataUltimaSessaoTermino { get; set; }
+        public string DataUltimaSessaoBloqueio { get; set; }
+        public string DataUltimaSessaoDesbloqueio { get; set; }
+        public string DataUltimaSessaoEliminacao { get; set; }
+        public string DataUltimaSessaoRecuperacao { get; set; }
+        public string DataUltimaSessaoAtivacao { get; set; }
+        public string DataUltimaSessaoDesativacao { get; set; }
+        public string DataUltimaSessaoLogin { get; set; }
+        public string DataUltimaSessaoLogout { get; set; }
+        public string DataUltimaSessaoPassword { get}
+
+
+    
+    
+    }
 }
