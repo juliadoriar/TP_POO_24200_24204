bf2543a3afb233d0a1a2ee021ccd6db8b3de3221
juliadoriar <juliadoriarodrigues@gmail.com>
2023-11-09 03:54:41 +0000

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_POO_24200_24204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    } 
+}
---
+++ Utilizador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_POO_24200_24204
+{
+    internal class Utilizador
+    {
+        
+    } 
+}
